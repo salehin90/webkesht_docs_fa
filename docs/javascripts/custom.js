e12f047ca553cf434837db856f24c49b3ede32cf
sa <PC3\HP>
2025-10-08 13:13:08 +0330

    1404-07-16. Custom-Formula-Added

--- docs/javascripts/custom.js
+++ docs/javascripts/custom.js
@@ -25,3 +25,18 @@
                 header.classList.add('collapsed');
             }
         }
+
+        // بارگذاری MathJax برای نمایش فرمول‌های ریاضی
+        window.MathJax = {
+            tex: {
+                inlineMath: [['$', '$']],
+                displayMath: [['$$', '$$']]
+            }
+        };
+
+        (function() {
+            var script = document.createElement('script');
+            script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
+            script.async = true;
+            document.head.appendChild(script);
+        })();
